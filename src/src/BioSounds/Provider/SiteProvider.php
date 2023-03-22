bb7702095d9e9dd101ae8193ad01e562242011e7
liudilong <liudilong199362@gmail.com>
2023-03-22 19:58:07 +0800

    Tensorflow

--- src/src/BioSounds/Provider/SiteProvider.php
+++ src/src/BioSounds/Provider/SiteProvider.php
@@ -74,7 +74,7 @@ class SiteProvider extends BaseProvider
             $sql .= " AND c.collection_id = $collectionId ";
         }
         if ($search) {
-            $sql .= " AND CONCAT(s.site_id, s.name, s.longitude_WGS84_dd_dddd, s.latitude_WGS84_dd_dddd, s.topography_m, s.freshwater_depth_m, e1.name, e2.name, e3.name) LIKE '%$search%' ";
+            $sql .= " AND CONCAT(IFNULL(s.site_id,''), IFNULL(s.name,''), IFNULL(s.longitude_WGS84_dd_dddd,''), IFNULL(s.latitude_WGS84_dd_dddd,''), IFNULL(s.topography_m,''), IFNULL(s.freshwater_depth_m,''), IFNULL(e1.name,''), IFNULL(e2.name,''), IFNULL(e3.name,'')) LIKE '%$search%' ";
         }
         $sql .= " GROUP BY s.site_id ";
         $this->database->prepareQuery($sql);
@@ -96,7 +96,7 @@ class SiteProvider extends BaseProvider
             $sql .= " AND c.collection_id = $collectionId ";
         }
         if ($search) {
-            $sql .= " AND CONCAT(s.site_id, s.name, s.longitude_WGS84_dd_dddd, s.latitude_WGS84_dd_dddd, s.topography_m, s.freshwater_depth_m, e1.name, e2.name, e3.name) LIKE '%$search%' ";
+            $sql .= " AND CONCAT(IFNULL(s.site_id,''), IFNULL(s.name,''), IFNULL(s.longitude_WGS84_dd_dddd,''), IFNULL(s.latitude_WGS84_dd_dddd,''), IFNULL(s.topography_m,''), IFNULL(s.freshwater_depth_m,''), IFNULL(e1.name,''), IFNULL(e2.name,''), IFNULL(e3.name,'')) LIKE '%$search%' ";
         }
         $sql .= " GROUP BY s.site_id ";
         $a = ['s.site_id', 's.name', 's.longitude_WGS84_dd_dddd', 's.latitude_WGS84_dd_dddd', 's.topography_m', 's.freshwater_depth_m', 's.gadm0', 's.gadm1', 's.gadm2', 'e1.name', 'e2.name', 'e3.name'];
