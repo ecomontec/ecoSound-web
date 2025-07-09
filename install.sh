830b3bc040b4f68298a553de74f562394ce1015c
LiudilongNJ <liudilong199362@gmail.com>
2025-07-09 09:16:57 +0800

    Fixed incorrect comments.

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ docker exec -i "$(docker ps -q -f ancestor=mysql)" mysql -ubiosounds -pbiosounds
 docker exec -i "$(docker ps -q -f ancestor=mysql)" mysql -ubiosounds -pbiosounds biosounds < gadm.sql
 docker exec -i "$(docker ps -q -f ancestor=mysql)" mysql -ubiosounds -pbiosounds biosounds < world_seas.sql
 
-# Install BioSounds
+# Install BirdNET-Analyzer
 git clone https://github.com/kahst/BirdNET-Analyzer.git ./src/BirdNET-Analyzer
 cd ./src/BirdNET-Analyzer
 git checkout 9c2f852
